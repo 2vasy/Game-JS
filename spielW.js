11bc19f920f297abb6be40e42180ef8ded53bf3d
2vasy <vasiliykulesh111@gmail.com>
2023-01-25 21:06:26 +0100

    empty fields

--- spielW.js
+++ spielW.js
@@ -12,32 +12,47 @@ let item = 0;
 let userNum;
 let randNum;
 
-startBtn.onclick = function randonGenerator() {
+startBtn.onclick = function randonGenerator(event) {
+    event.preventDefault();
     let ersteGrenze = Math.ceil(first.value);
     let zweiteGrenze = Math.floor(second.value);
     randNum = Math.floor(Math.random() * (zweiteGrenze - ersteGrenze + 1)) + ersteGrenze;
+    if(ersteGrenze == 0 || zweiteGrenze == 0){
+        alert("Fill in all empty fields!!!")
+        return;
+    }
     los.textContent = "Los Geht's";
     return randNum;
 };
 
-btn.onclick = function () {
+btn.onclick = function (event) {
+    event.preventDefault();
     userNum = input.value;
+    if(randNum !== 0 && userNum !== undefined) {
+        
+        console.log(userNum, randNum);
+        if (userNum > randNum) {
+            check.textContent = "nicht erraten";
+            help.textContent = "Zahl ist zu groß";
+            item++;
+            count.textContent = item;
+        } else if (userNum < randNum) {
+            check.textContent = "nicht erraten";
+            help.textContent = "Zahl ist zu klein";
+            item++;
+            count.textContent = item;
+        } else {
+            check.textContent = "Glückwünsche! du, hast die Nummer erraten";
+            help.textContent = "Genau richtig";
+            item++;
+            count.textContent = item;
+        }
 
-    console.log(userNum, randNum);
-    if (userNum > randNum) {
-        check.textContent = "nicht erraten";
-        help.textContent = "Zahl ist zu groß";
-        item++;
-        count.textContent = item;
-    } else if (userNum < randNum) {
-        check.textContent = "nicht erraten";
-        help.textContent = "Zahl ist zu klein";
-        item++;
-        count.textContent = item;
-    } else {
-        check.textContent = "Glückwünsche! du hast die Nummer erraten";
-        help.textContent = "Genau richtig";
-        item++;
-        count.textContent = item;
+    }else {
+
+        alert("Fill in all empty fields!!!");
+        return;
     }
+
 };
+    
